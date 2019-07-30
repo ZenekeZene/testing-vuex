50c9e723147b19595d1cfc141a54885c18f8d514
Héctor <hvillar@theinit.com>
2019-07-30 14:18:59 +0200

    1.- Simple example

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+	{{ count }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  computed: {
+	  count() {
+		  return this.$store.state.count;
+	  }
+  },
+
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+})
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store,
 }).$mount("#app");
